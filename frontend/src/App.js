230056413782697c3274115ff15787be132501fc
renda28 <renzel.david@gmail.com>
2022-06-30 19:58:06 +0100

    create initial frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+//pages and components
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
